f66f7ea26f4de1df5e62c56682c9b1a2f1bb576b
Peter Hironaka <peterhironaka@Peters-iMac-2.home>
2015-03-17 09:54:03 -0700

    Intergalactic
    TMC

---
+++ js/videos.js
@@ -0,0 +1,25 @@
+var videos = [
+  
+
+    {
+      title: "Sprint Crossover Oladipo",
+      url: "/videos/sprint-crossover-oladipo",
+      slug: "sprint-crossover-oladipo",
+      youtube: "RoG7rNv6duI"
+    },
+    
+    {
+      title: "Intergalactic Travels",
+      url: "/videos/intergalactic-travels",
+      slug: "intergalactic-travels",
+      youtube: "lOaF1k9Nz0k"
+    },
+    
+    {
+      title: "Chazebralope BTS",
+      url: "/videos/chazebralope-bts",
+      slug: "chazebralope-bts",
+      youtube: "z1U3MUsNszA"
+    },
+    
+];
